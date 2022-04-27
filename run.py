408cfcbf51020112e643bd2d2cd0123c7ae9eb85
ZippMukami <zimukami@gmail.com>
2022-04-27 06:13:12 +0300

    creates account function.

--- run.py
+++ run.py
@@ -1,1 +1,13 @@
 #!/usr/bin/env python3.8
+from user import User
+
+
+#create account function
+def create_user(user_name, password, phone_number, email):
+      '''
+      Function to create a new user
+      '''
+      new_user = User (user_name, password, phone_number, email)
+      return new_user
+
+
